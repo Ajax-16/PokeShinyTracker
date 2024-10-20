28fbb53aadb190b5223770fb944088b8c224edb6
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-10-20 18:22:37 +0200

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron');
+
+app.whenReady().then(()=>{
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadFile('./views/index.html');
+})
